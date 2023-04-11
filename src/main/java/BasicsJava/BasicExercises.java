5832e866c2b4a43e660701b6769e7aaedef6bafc
saidBoudad <saidBoudad35@gmail.com>
2023-04-11 03:44:43 +0000

    First exercise even odd

--- src/main/java/BasicsJava/BasicExercises.java
+++ src/main/java/BasicsJava/BasicExercises.java
@@ -2,13 +2,13 @@ package BasicsJava;
 
 import java.util.Scanner;
 
-public class NewClass {
+public class BasicExercises {
     public static void main(String[] args) {
-       Integer number,remainder;
+        //exercise of even and odd
+       int number;
        System.out.println("please enter a number : ");
        Scanner scan = new Scanner(System.in);
        number = scan.nextInt();
-       remainder = number % 2;
        System.out.println("number " + number + " is an " + (((number % 2) == 0) ? " even " : " odd ") + " number");
     }
 }
