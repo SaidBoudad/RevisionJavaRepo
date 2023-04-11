8960dad6a8f693181719d5235554741fd9740b30
saidBoudad <saidBoudad35@gmail.com>
2023-04-11 23:57:36 +0000

    second exercise addition

--- src/main/java/BasicsJava/BasicExercises.java
+++ src/main/java/BasicsJava/BasicExercises.java
@@ -10,5 +10,13 @@ public class BasicExercises {
        Scanner scan = new Scanner(System.in);
        number = scan.nextInt();
        System.out.println("number " + number + " is an " + (((number % 2) == 0) ? " even " : " odd ") + " number");
+       //2 add two numbers
+        int number1,number2,result;
+        System.out.println("please enter 1st number");
+        number1 = scan.nextInt();
+        System.out.println("please enter 2nd number");
+        number2 = scan.nextInt();
+        result = number1 + number2;
+        System.out.println("the addition of the two numbers is " + result);
     }
 }
