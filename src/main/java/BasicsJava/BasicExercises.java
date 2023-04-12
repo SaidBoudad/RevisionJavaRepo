db1e644fc4ada184c53eab88b5d70e2c520324cf
saidBoudad <saidBoudad35@gmail.com>
2023-04-12 05:18:47 +0000

    factorial using recursion method

--- src/main/java/BasicsJava/BasicExercises.java
+++ src/main/java/BasicsJava/BasicExercises.java
@@ -11,8 +11,8 @@ public class BasicExercises {
         Scanner scan = new Scanner(System.in);
         number = scan.nextInt();
         System.out.println("number " + number + " is an " + (((number % 2) == 0) ? " even " : " odd ") + " number");
-        
-        
+
+
         //2 add two numbers
         int number1, number2, result;
         System.out.println("please enter 1st number");
@@ -21,8 +21,8 @@ public class BasicExercises {
         number2 = scan.nextInt();
         result = number1 + number2;
         System.out.println("the addition of the two numbers is " + result);
-        
-        
+
+
         //3 swap two numbers
         int numberA, numberB, temp;
         System.out.println("please enter 1st number");
@@ -34,8 +34,8 @@ public class BasicExercises {
         numberB = numberA;
         numberA = temp;
         System.out.printf("the swaped numbers are \n number1 = %d \n number2 = %d \n", numberA, numberB);
-        
-        
+
+
         //4 largest number among 3 numbers by using if else
         int number_a, number_b, number_c, largest = 0;
         System.out.println("please enter 1st number ");
@@ -63,28 +63,28 @@ public class BasicExercises {
         n3 = scan.nextInt();
         if (n1 == n3 && n2 == n1 && n1 == n2) {
             System.out.println("the three numbers are equal");
-        } else largestNumber = (n1>=n2 && n1>=n3 ? n1 : (n2>=n1 && n2>=n3 ? n2 : n3));
+        } else largestNumber = (n1 >= n2 && n1 >= n3 ? n1 : (n2 >= n1 && n2 >= n3 ? n2 : n3));
         System.out.printf("the largest number among %d , %d and %d is %d", n1, n2, n3, largestNumber);
 
         //6 program multiplalier from 1 to 10 any number given by user
         int num;
         System.out.println("enter a number");
         num = scan.nextInt();
-        for (int i=1;i<=10;i++){
-            System.out.printf("%d * %d = %d \n",num,i,(num*i));
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d = %d \n", num, i, (num * i));
         }
 
         //7 find all divisors of a number
-        int numToDivison=0;
+        int numToDivison = 0;
         System.out.println("enter a number please");
         numToDivison = scan.nextInt();
-        System.out.printf("the divisors of %d are : ",numToDivison);
-        for (int i=1;i<=numToDivison/2;i++){
-            if (numToDivison%i==0){
-                System.out.printf(i +" , ");
+        System.out.printf("the divisors of %d are : ", numToDivison);
+        for (int i = 1; i <= numToDivison / 2; i++) {
+            if (numToDivison % i == 0) {
+                System.out.printf(i + " , ");
             }
         }
-        System.out.printf(numToDivison +" .");
+        System.out.printf(numToDivison + " .");
 
         //8 factorial of number
         int Number;
@@ -113,7 +113,16 @@ public class BasicExercises {
             }
             System.out.printf("the factorial of %d! is %d", Number1, Factorial);
         }
-    
 
+        //10 factorial using recursion method
+        int numberone;
+        System.out.println("please enter a number : ");
+        numberone = scan.nextInt();
+        System.out.printf("the factroial of the number %d! is : %d", numberone, factorialone(numberone));
+    }
+    public static int factorialone ( int n){
+        if (n <= 1) {
+            return 1;
+        } else return n * factorialone(n - 1);
     }
 }
