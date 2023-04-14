774386b231c9ac9205019256e0ea43a406fbbcff
saidBoudad <saidBoudad35@gmail.com>
2023-04-14 06:12:18 +0000

    Sum Of Numbers Between Two

---
+++ src/main/java/BasicsJava/TicTacToeGame.java
@@ -0,0 +1,21 @@
+package BasicsJava;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        char[][] gameBoard = {{" "},{|},{" "},{|},{" "},
+        {"_"},{"+"},{"_"},{"+"},{"_"},
+        {" "},{|},{" "},{|},{" "},
+        {"_"},{"+"},{"_"},{"+"},{"_"},
+        {" "},{|},{" "},{|},{" "}}
+
+    }
+    public static void printGame(char[][] gameBoard){
+
+    }
+    public static void placeChooise(char[][] gameBoard,int position,String user){
+
+    }
+    public static String checkWinner(){
+        return null;
+    }
+}
